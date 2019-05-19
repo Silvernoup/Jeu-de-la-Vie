28c671129d8a44fc1682bd2319db44a427ecd312
Silvernoup <franckklaj@hotmail.fr>
2019-05-19 20:46:03 +0200

    change screen width

--- constantes.h
+++ constantes.h
@@ -2,7 +2,7 @@
 #define DEF_CONSTANTES
 
 #define LARGEUR_FENETRE 1200    // Largeur de la  fenetre principale 
-#define HAUTEUR_FENETRE 600     // Hauteur de la fenetre principale
+#define HAUTEUR_FENETRE 650     // Hauteur de la fenetre principale
 
 
 #endif
