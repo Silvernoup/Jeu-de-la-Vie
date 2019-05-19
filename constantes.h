4b5ec9491f9daf3f283b1ed861505162b28d8f6b
Silvernoup <franckklaj@hotmail.fr>
2019-05-19 20:25:25 +0200

    project init

---
+++ constantes.h
@@ -0,0 +1,8 @@
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
+
+#define LARGEUR_FENETRE 1200    // Largeur de la  fenetre principale 
+#define HAUTEUR_FENETRE 600     // Hauteur de la fenetre principale
+
+
+#endif
